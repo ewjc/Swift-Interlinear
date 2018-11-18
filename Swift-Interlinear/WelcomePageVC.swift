d357909c451ac307827d0c8fe979b3ff1a9f4c8a
Eric Wong <ericswong7@gmail.com>
2018-11-17 21:21:54 -0800

    page controller swipes working 3 seperate vcs

--- Swift-Interlinear/WelcomePageVC.swift
+++ Swift-Interlinear/WelcomePageVC.swift
@@ -8,72 +8,76 @@
 
 import UIKit
 
-class WelcomePageVC: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+class WelcomePageVC: UIPageViewController {
     
     lazy var orderedViewControllers: [UIViewController] = {
         let vcOne = WelcomePageOne()
         let vcTwo = WelcomePageTwo()
+        let vcThree = WelcomePageThree()
 
-        return [vcOne]
+        return [vcOne, vcTwo, vcThree]
     }()
     
     let pageControl = UIPageControl()
     
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        if let viewControllerIndex = self.orderedViewControllers.index(of: viewController) {
-            if viewControllerIndex == 0 {
-                // wrap to last page in array
-                return self.orderedViewControllers.last
-            } else {
-                // go to previous page in array
-                return self.orderedViewControllers[viewControllerIndex - 1]
-            }
-        }
-        
-        return nil
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        if let viewControllerIndex = self.orderedViewControllers.index(of: viewController) {
-            if viewControllerIndex < self.orderedViewControllers.count - 1 {
-                // go to next page in array
-                return self.orderedViewControllers[viewControllerIndex + 1]
-            } else {
-                // wrap to first page in array
-                return self.orderedViewControllers.first
-            }
-        }
-        return nil
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
-        
-        // set the pageControl.currentPage to the index of the current viewController in pages
-        if let viewControllers = pageViewController.viewControllers {
-            if let viewControllerIndex = self.orderedViewControllers.index(of: viewControllers[0]) {
-                self.pageControl.currentPage = viewControllerIndex
-            }
-        }
-    }
-    
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.delegate = self
         self.dataSource = self
-        let initialPage = 0
-    
-
-        setViewControllers([orderedViewControllers[initialPage]], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
         
+        if let firstVC = orderedViewControllers.first {
+            setViewControllers([firstVC], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
+        }
+        
+        configurePageControl()
+    }
+    
+    fileprivate func configurePageControl() {
         pageControl.currentPageIndicatorTintColor = UIColor.black
         pageControl.pageIndicatorTintColor = UIColor.lightGray
-        pageControl.currentPage = initialPage
         
         view.addSubview(pageControl)
-        pageControl.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 40, paddingBottom: 10, paddingRight: 40, width: 0, height: 0)
+        pageControl.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 40, paddingBottom: -10, paddingRight: 40, width: 0, height: 0)
     }
+    
+}
 
+extension WelcomePageVC: UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = self.orderedViewControllers.index(of: viewController) else { return nil }
+        
+        if viewControllerIndex == 0 {
+            // wrap to last page in array
+            return nil
+        } else {
+            // go to previous page in array
+            return self.orderedViewControllers[viewControllerIndex - 1]
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = self.orderedViewControllers.index(of: viewController) else { return nil }
+        
+            if viewControllerIndex < self.orderedViewControllers.count - 1 {
+                // go to next page in array
+                return self.orderedViewControllers[viewControllerIndex + 1]
+            } else {
+                // wrap to first page in array
+                return nil
+            }
+
+        
+    }
+//
+//    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+//
+//        // set the pageControl.currentPage to the index of the current viewController in pages
+//        if let viewControllers = pageViewController.viewControllers {
+//            if let viewControllerIndex = self.orderedViewControllers.index(of: viewControllers[0]) {
+//                self.pageControl.currentPage = viewControllerIndex
+//            }
+//        }
+//    }
 }
 
