e24ef6efd9eb305d996829ab9d007953a22dc7bb
Eric Wong <ericswong7@gmail.com>
2018-11-20 21:03:00 -0800

    table view programmatic setup functioning

---
+++ Swift-Interlinear/MenuCell.swift
@@ -0,0 +1,23 @@
+//
+//  MenuCell.swift
+//  Swift-Interlinear
+//
+//  Created by Eric Wong on 11/20/18.
+//  Copyright © 2018 Eric Wong. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
--- Swift-Interlinear/MenuVC.swift
+++ Swift-Interlinear/MenuVC.swift
@@ -9,9 +9,59 @@
 import UIKit
 
 class MenuVC: UIViewController {
+    
+    let menuArray = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1 Corinthians", "2 Corinthians", "Galatians", "Ephesians", "Philipians", "Colossians", "1 Thessalonians", "2 Thessalonians", "1 Timothy", "2 Timothy", "Titus", "Philemon", "Hebrews", "James", "1 Peter", "2 Peter", "1 John", "2 John", "3 John", "Jude", "Revelation"]
+    
+    var menuTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        navigationItem.title = "The scripture"
+        
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+        
+        menuTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+        
+        menuTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MenuCell")
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        view.addSubview(menuTableView)
+        menuTableView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
     }
 
+}
+
+extension MenuVC: UITableViewDataSource, UITableViewDelegate {
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if section == 0 {
+//            return 1
+//        } else {
+//            return menuArray.count
+//        }
+
+        return menuArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MenuCell", for: indexPath)
+        
+        cell.textLabel?.text = menuArray[indexPath.row]
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 24)
+        cell.textLabel?.textColor = UIColor.darkGray
+        
+        return cell
+    }
+    
+    
 }
