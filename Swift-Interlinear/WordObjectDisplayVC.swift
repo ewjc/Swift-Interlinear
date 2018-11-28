404c73f8b4d74497bc76974417c64774c844c30c
Eric Wong <ericswong7@gmail.com>
2018-11-28 00:43:06 -0800

    updated view change for word object

--- Swift-Interlinear/WordObjectDisplayVC.swift
+++ Swift-Interlinear/WordObjectDisplayVC.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class WordObjectDisplayVC: UIViewController {
     
+    let cancelButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(#imageLiteral(resourceName: "cancel_button"), for: .normal)
+        
+        return btn
+    }()
+    
     let grayView: UIView = {
         let v = UIView()
-        v.backgroundColor = UIColor.lightGray
+        v.backgroundColor = UIColor(white: 0.3, alpha: 0.7)
         
         return v
     }()
@@ -20,6 +27,8 @@ class WordObjectDisplayVC: UIViewController {
     let wordObjectContainerView: UIView = {
         let v = UIView()
         v.backgroundColor = UIColor.white
+        v.layer.cornerRadius = 7.0
+        v.clipsToBounds = true
         
         return v
     }()
@@ -27,11 +36,15 @@ class WordObjectDisplayVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor.clear
         view.addSubview(grayView)
         grayView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
         grayView.addSubview(wordObjectContainerView)
-        wordObjectContainerView.anchor(top: grayView.topAnchor, left: grayView.leftAnchor, bottom: grayView.bottomAnchor, right: grayView.rightAnchor, paddingTop: 40, paddingLeft: 50, paddingBottom: 40, paddingRight: 50, width: 0, height: 0)
+        wordObjectContainerView.anchor(top: grayView.topAnchor, left: grayView.leftAnchor, bottom: grayView.bottomAnchor, right: grayView.rightAnchor, paddingTop: 70, paddingLeft: 50, paddingBottom: 70, paddingRight: 50, width: 0, height: 0)
+        
+        grayView.addSubview(cancelButton)
+        cancelButton.anchor(top: grayView.topAnchor, left: grayView.leftAnchor, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: 40, height: 40)
         
     }
     
